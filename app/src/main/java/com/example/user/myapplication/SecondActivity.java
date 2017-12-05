dcde749ea5ffe8a45760e9ffcdeca61490b72fda
unknown <profile2697@gmail.com>
2017-12-06 00:10:40 +0900

    Learn Andriod Activity Lifecycle

--- app/src/main/java/com/example/user/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/user/myapplication/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +10,43 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Toast.makeText(this, "[B] onCreate() 함수 호출", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "[B] onStart() 함수 호출", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "[B] onRestart() 함수 호출", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "[B] onResume() 함수 호출", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "[B] onPause() 함수 호출", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "[B] onStop() 함수 호출", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "[B] onDestroy() 함수 호출", Toast.LENGTH_SHORT).show();
     }
 }
